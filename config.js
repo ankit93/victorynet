5c40867962c32aeee07c825bbf20ad68e7155dc1
ankit93 <aannkkiitt93@gmail.com>
2014-06-30 11:10:33 +0530

    ss

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 //database connection
 
 var client = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'vnt',
-database:'victoryNet'
+host: 'us-cdbr-east-06.cleardb.net',
+user: 'b923e5879b2598',
+password: 'c48c0cf4',
+database:'heroku_5035f62fbd8a42c'
 });
 
  return client;
